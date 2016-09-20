413421f4468a10fdfe02ed92904c798ca520225c
[lastwalker] <[syedhamdansher@gmail.com]>
2016-09-20 13:07:38 -0500

    Real commit 3

---
+++ storage_mgr.c
@@ -0,0 +1,10 @@
+#include <studio.h>
+#include <conio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+
+#include "storage_mgr.h"
+#include "dberror.h"
