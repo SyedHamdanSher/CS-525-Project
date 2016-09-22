07e3298801b35fea6e2f777e8c34fc423ddb467b
[lastwalker] <[syedhamdansher@gmail.com]>
2016-09-22 15:12:05 -0500

    real commit 6

--- storage_mgr.c
+++ storage_mgr.c
@@ -8,4 +8,57 @@
 
 #include "storage_mgr.h"
 #include "dberror.h"
-//Team 22
+
+/* manipulating page files */
+
+void initStorageManager(void) { //initialize Storage manager
+
+}
+
+RC createPageFile(char * fileName) { //create Page
+
+}
+RC openPageFile(char *fileName, SM_FileHandle *fHandle) {
+
+}
+
+RC closePageFile (SM_FileHandle *fHandle) {
+
+}
+
+RC destroyPageFile (char *fileName) {
+
+}
+
+RC readBlock (int pageNum, SM_FileHandle *fHandle, SM_PageHandle memPage) {
+
+}
+
+int getBlockPos (SM_FileHandle *fHandle) {
+
+}
+
+RC readFirstBlock (SM_FileHandle *fHandle, SM_PageHandle memPage) {
+
+}
+RC readPreviousBlock (SM_FileHandle *fHandle, SM_PageHandle memPage) {
+
+}
+RC readCurrentBlock (SM_FileHandle *fHandle, SM_PageHandle memPage) {
+
+}
+RC readNextBlock (SM_FileHandle *fHandle, SM_PageHandle memPage) {
+
+}
+RC readLastBlock (SM_FileHandle *fHandle, SM_PageHandle memPage) {
+
+}
+
+RC writeBlock (int pageNum, SM_FileHandle *fHandle, SM_PageHandle memPage) {
+
+}
+
+RC ensureCapacity(int numOfPages, SM_FileHandle *fHandle) {
+
+}
+
